a68a5646b63931e3fcae62cafc9236994d198a49
Manuel Jesus Flores Montano <mjflomon@alu.upo.es>
2022-11-28 23:24:42 +0100

    Vista ARTICULO de lista creada

--- __manifest__.py
+++ __manifest__.py
@@ -25,8 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/articulos.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
