8549fa104d7be9221b71d3cb57f7022634a2fc4e
Pedro Jesus Lazaro Diaz <pedrojesusld@gmail.com>
2022-12-02 18:50:38 +0100

    Basicamente preparar el main para que funcione con el codigo de la ultima entrega

--- __manifest__.py
+++ __manifest__.py
@@ -1,19 +1,4 @@
 # -*- coding: utf-8 -*-
-<<<<<<< HEAD
-{
-    'name': "upobebe",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-=======
 # Version:      0.1.0
 # Modelo:       Transaccion
 # Editor:       Manuel Jesus Flores Montano (@manueljesus00) and Pedro Jesus Lazaro Diaz (@vitalsum)
@@ -30,24 +15,11 @@
 
     'author': "Manuel Jesus Flores Montano",
     'website': "https://www.github.com/manueljesus00/upobebe",
->>>>>>> entrega24112022
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-<<<<<<< HEAD
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-=======
     'version': '0.1.0',
     'depends': ['base'],
     'data': [
@@ -58,7 +30,6 @@
         #'views/tipoTransaccion_view.xml',
         'views/Transaccion.xml',
         'views/menu.xml'
->>>>>>> entrega24112022
     ],
     # only loaded in demonstration mode
     'demo': [
--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
-<<<<<<< HEAD
-
-from . import models
-=======
 from . import Transaccion
 from . import cliente
 from . import suscripcion
 #from . import tipoTransaccion
 #from . import empleados
->>>>>>> entrega24112022
