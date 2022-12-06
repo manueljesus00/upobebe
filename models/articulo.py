fe967119096511c3238b93604b11ecaf2d8038d3
Pedro Jesus Lazaro Diaz <pedrojesusld@gmail.com>
2022-12-06 13:49:07 +0100

    Modelo estadoArticulo & modificacion modelo articulo

--- models/articulo.py
+++ models/articulo.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+# Version:      0.2.0
+# Modelo:       Articulo
+# Editor:       Pedro Jesus Lazaro Diaz (@vitalsum) and Manuel Jesus Flores Montaño (@manueljesus00)
+# Fecha rev:    06/12/2022
+
 from odoo import models, fields, api
 
 
@@ -8,7 +13,9 @@ class articulo(models.Model):
     _description = 'upobebe.articulo'
 
     idArticulo = fields.Integer(required=True, readonly=True, copy=True, index=True, string="ID Articulo")
-    numAlmacen = fields.Many2one("upobebe.almacen","Numero de almacen", required=True)
-    tipoEstado = fields.Many2one("upobebe.estado","Estado del producto", required=True)
-    nombreProducto = fields.Many2one("upobebe.articulo","Tipo de articulo", required=True)
+    photo = fields.Binary('Photo')
     precio = fields.Float(size=6, required=True)
+
+    # numAlmacen = fields.Many2one("upobebe.almacen","Numero de almacen", required=True)
+    tipoEstado = fields.Many2one("upobebe.estadoArticulo", required=True, string="Estado")
+    # nombreProducto = fields.Many2one("upobebe.articulo","Tipo de articulo", required=True)
